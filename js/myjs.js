62e8174d70c8f28cfbd9d126ae6caea02b104e4c
kahn-aar <nicolas.martinr@gmail.com>
2014-11-23 15:45:52 -0500

    code html

--- js/myjs.js
+++ js/myjs.js
@@ -1,87 +1,60 @@
-    var phonecatApp = angular.module('phonecatApp', []);
-     
-    phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-        function ($scope, $http) {
-            $http.get('./ressources/json/types.json').success(function(data) {
-                $scope.listeType = data;
-                console.log(data);
-            });
-            $http.get('./ressources/json/uv.json').success(function(data) {
-                $scope.listuv = data;
-            });
-            $http.get('./ressources/json/contenu.json').success(function(data) {
-                $scope.contenu = data;
-            });
-            $http.get('./ressources/json/xp.json').success(function(data) {
-                $scope.xp = data;
-            });
-            $http.get('./ressources/json/otherXp.json').success(function(data) {
-                $scope.otherXp = data;
-            });
-            $http.get('./ressources/json/skills.json').success(function(data) {
-                $scope.skills = data;
-            });
-            $http.get('./ressources/json/projets.json').success(function(data) {
-                $scope.projets = data;
-            });
-            $http.get('./ressources/json/informations_generales.json').success(function(data) {
-                $scope.infos = data;
-            });
-        }
-    ]);
 
-    $scope.query="";
 
     function displayUv() {
-        hideAll();
-        $('.uv').show(); 
-        $('.uvBtn').addClass("active");
+        //hideAll();
+        //$('.uv').show(); 
+        //$('.uvBtn').addClass("active");
     }
 
     function displayContent() {
-        hideAll();
-        $('.cont').show();
-        $('.descriptionBtn').addClass("active");
+        //hideAll();
+        //$('.cont').show();
+        //$('.descriptionBtn').addClass("active");
     }
 
     function displayXp() {
-        hideAll();
-        $('.xp').show(); 
-        $('.xpBtn').addClass("active");
+        //hideAll();
+        //$('.xp').show(); 
+        //$('.xpBtn').addClass("active");
 
     }
 
     function displayOtherXp() {
-        hideAll();
-        $('.other-xp').show(); 
-        $('.otherXpBtn').addClass("active");
+        //hideAll();
+       //$('.other-xp').show(); 
+        //$('.otherXpBtn').addClass("active");
     }
 
     function displaySkills() {
-        hideAll();
+        //hideAll();
         $('.skills').show(); 
         $('.skillBtn').addClass("active");
     }
 
     function displayProjets() {
-        hideAll();
+        //hideAll();
         $('.projets').show(); 
         $('.projectBtn').addClass("active");
     }
 
+    function display() {
+        hideAll();
+        alert($(this).attr("id"));
+        $(this).addClass('active');
+        document.getElementById("team1").innerHTML="courgette";
+        document.getElementById("team2").innerHTML="Tomatte";
+        document.getElementById("period").innerHTML="2";
+        document.getElementById("timestamp").innerHTML="1:75";
+        document.getElementById("score1").innerHTML="2";
+        document.getElementById("score2").innerHTML="1";
+        document.getElementById("penal1").innerHTML="2";
+        document.getElementById("penal2").innerHTML="1";
+
+
+    }
+
     function hideAll() {
-        $('.uv').hide(); 
-        $('.cont').hide();
-        $('.xp').hide();
-        $('.other-xp').hide();
-        $('.skills').hide();
-        $('.projets').hide();
-        $('.descriptionBtn').removeClass("active");
-        $('.uvBtn').removeClass("active");
-        $('.xpBtn').removeClass("active");
-        $('.otherXpBtn').removeClass("active");
-        $('.skillBtn').removeClass("active");
-        $('.projectBtn').removeClass("active");
+        $('.buttons').removeClass("active");
 
     }
 
